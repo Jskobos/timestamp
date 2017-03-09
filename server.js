a22ff7ae1cf1ed6481ec302efb8fb4b0c6373a18
Jared Kobos <jaredkobos@gmail.com>
2017-03-09 13:11:15 +0800

    Invalid case handled

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 var express = require('express');
+var moment  = require('moment');
+var app = express();
+
+app.get('/', function(req,res) {
+  res.send("Welcome screen");
+});
+
+app.get('/:datestring', function (req, res) {
+  if (!moment(req.params.datestring).isValid()) {
+    res.json({unix:null,natural:null});
+  }
+  res.end();
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
---
+++ spec/server.spec.js
@@ -0,0 +1,54 @@
+var request = require("request");
+
+var base_url = "http://localhost:3000/"
+
+var simple_query = {
+  input: 1450137600,
+  output: {"unix":1450137600,"natural":"December 15, 2015"}
+}
+var string_query = {
+  input: 'December%2015,%202015',
+  output: {"unix":1450137600,"natural":"December 15, 2015"}
+}
+
+var string_query2 = {
+  input: '01%January%1988',
+  output: {"unix":1450137600,"natural":"December 15, 2015"}
+}
+
+var invalid_query = {
+  input: "invalid",
+  output: {
+    "unix": null,
+    "natural": null
+  }
+};
+
+describe("Timestamp server", function() {
+  describe("/GET", function() {
+    it("returns a status code 200", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+    it("accepts a query string", function(done) {
+      request.get(base_url + simple_query.input, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+    it("returns a null JSON object if the query is not a valid date", function(done) {
+      request.get(base_url + invalid_query.input, function(error, response, body) {
+        expect(JSON.parse(body)).toEqual(invalid_query.output);
+        done();
+      });
+    });
+    it("returns Unix and natural datestrings", function(done) {
+      request.get(base_url + simple_query.input, function(error, response, body) {
+        expect(JSON.parse(body)).toEqual(simple_query.output);
+        done();
+      });
+    });
+  });
+});
